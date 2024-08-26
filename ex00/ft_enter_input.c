d8ec37aaa0d028355c42ad3b0c3fe24d80e5ee8e
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2024-08-26 10:34:52 -0300

    rush-01 correction post-evaluation

--- ex00/ft_enter_input.c
+++ ex00/ft_enter_input.c
@@ -6,7 +6,7 @@
 /*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 15:45:46 by jarao-de          #+#    #+#             */
-/*   Updated: 2024/08/25 15:46:48 by jarao-de         ###   ########.fr       */
+/*   Updated: 2024/08/26 10:27:15 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ int	*ft_enter_input(char *input, int size)
 	jump = 0;
 	while (input[index] != '\0')
 	{
-		if (input[index] >= '0' && input[index] <= '9')
+		if (input[index] >= '0' && input[index] <= '9'
+			&& (input[index + 1] == ' ' || input[index + 1] == '\0'))
 		{
 			num_input[jump] = input[index] - '0';
 			jump = jump + 1;
